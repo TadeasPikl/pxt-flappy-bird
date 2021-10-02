54f6d4331d41d178bfed49f3d1eec84309c52380
Tadeáš Pikl <91247702+TadeasPikl@users.noreply.github.com>
2021-10-02 17:48:25 +0200

    version 0.2.1 - made the game (kinda work)

--- main.ts
+++ main.ts
@@ -8,18 +8,23 @@ input.onButtonPressed(Button.A, function () {
 })
 function Wall_Move () {
     Wall_0.change(LedSpriteProperty.X, -1)
-    Wall_1.change(LedSpriteProperty.X, -1)
-    Wall_2.change(LedSpriteProperty.X, -1)
     Wall_3.change(LedSpriteProperty.X, -1)
     Wall_4.change(LedSpriteProperty.X, -1)
 }
+function Reset_wall () {
+    Wall_0 = game.createSprite(4, 0)
+    Wall_3 = game.createSprite(4, 3)
+    Wall_4 = game.createSprite(4, 4)
+    Wall_0.delete()
+    Wall_3.delete()
+    Wall_4.delete()
+}
 let Wall_4: game.LedSprite = null
 let Wall_3: game.LedSprite = null
-let Wall_2: game.LedSprite = null
-let Wall_1: game.LedSprite = null
 let Wall_0: game.LedSprite = null
 let rng = 0
 let bird: game.LedSprite = null
+Reset_wall()
 let wall_time = 0
 bird = game.createSprite(1, 2)
 while (true) {
@@ -31,14 +36,16 @@ while (true) {
             Whole_Wall_1()
         }
     } else {
-        if (Wall_0.isTouchingEdge()) {
-            Wall_0.delete()
-            Wall_1.delete()
-            Wall_2.delete()
-            Wall_3.delete()
-            Wall_4.delete()
-        } else {
-            Wall_Move()
+        if (!(Wall_0.isDeleted())) {
+            if (wall_time == 5) {
+                Wall_0.delete()
+                Wall_3.delete()
+                Wall_4.delete()
+            } else {
+                if (!(Wall_0.isDeleted())) {
+                    Wall_Move()
+                }
+            }
         }
     }
     bird.change(LedSpriteProperty.Y, 1)
